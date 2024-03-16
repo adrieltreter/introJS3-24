3f632c4e63cc17ff8fb5de67b207d6516ee2451a
@adriel_abner397 <126088211+adrieltreter@users.noreply.github.com>
2024-03-15 22:10:49 -0300

    Add files via upload

---
+++ introJS3-24-main/introcaojs-3-24/1_tipos_primitivos/script.js
@@ -0,0 +1,22 @@
+//tipos number
+
+const text1 = "ola mundo";
+
+const text2 = 'ola mundoi';
+
+const idadsenha = "senhassupersegu";
+
+const stringDeNumeros = "3642"
+//concatenacao
+const citacao ="meu nome é"
+const meuNome ="adriel abner treter"
+
+
+console.log(citacao+meuNome);
+//comparacao
+
+console.log(text1 == text2);
+const n1 =1;
+const n2=2;
+
+console.log(n1 == n2);
